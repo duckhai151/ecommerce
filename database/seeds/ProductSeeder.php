7de08e24167320e778ffd2d3211c8e0ae6038adf
Duc Khai <duckhai151@gmail.com>
2019-06-05 23:05:56 +0700

    Update

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -14,7 +14,7 @@ class ProductSeeder extends Seeder
         DB::table('products')->insert([
         	['subcat_id'=>'1','name'=>'Latitute 7450','img'=>'latitute7450.jpg'],
         	['subcat_id'=>'2','name'=>'Zenbook X123','img'=>'zenbook.jpg'],
-        	['subcat_id'=>'3','name'=>'Adidas UntralBoots','img'=>'ubtralboots.jpg'],
+        	['subcat_id'=>'3','name'=>'Adidas UntralBoots','img'=>'ultralboots.jpg'],
         	['subcat_id'=>'4','name'=>'Nike Air Max','img'=>'airmax.jpg'],
         	['subcat_id'=>'5','name'=>'Iphone X','img'=>'iphonex.jpg'],
         	['subcat_id'=>'6','name'=>'SS Galaxy S10','img'=>'s10.jpg'],
